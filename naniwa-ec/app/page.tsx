fee036dbc2868b71ff68a931a9e93d97fb856819
25r1116shimojo-oss <25r1116.shimojo@gmail.com>
2026-03-20 05:24:52 +0900

    server.tsとpage.tsxの修正

--- naniwa-ec/app/page.tsx
+++ naniwa-ec/app/page.tsx
@@ -1,27 +1,45 @@
-import { createClient } from "@/lib/supabase/server";
+import { createClient } from "@/lib/supabase/server" // インポート、外部から取り込む
 
-export default async function Home() {
-  const supabase = await createClient();
+export default async function Home() {                // 「Home」という名前の非同期関数をデフォルトでエクスポート
+  const supabase = await createClient()
+
   const { data: products, error } = await supabase
     .from("products")
     .select("*")
-    .order("created_at");
+    .order("created_at")
 
   if (error) {
-    return <div>エラーが発生しました: {error.message}</div>;
+    return (
+      <>
+        <div>エラーが発生しました: {error.message}</div>
+        <div>サンプルです。</div>
+      </>
+    )
+  } else {
+    return (
+      <main>
+        <h1 className="test">接続テスト</h1>
+
+        {products.map((product) => (
+          <div key={product.id}>
+            {product.name}{product.is_featured && ("★おすすめ")}<br />
+            ¥{product.price}／在庫:{product.stock}個
+          </div>
+        ))}
+      </main>
+    )
   }
-
-  return (
-    <main style={{ maxWidth: 800, margin: "0 auto", padding: 40 }}>
-      <h1>テスト</h1>
-
-      {products?.map((product) => (
-        <div key={product.id}>
-          <strong>{product.name}</strong><br />
-          ¥{product.price.toLocaleString()} ／ 在庫: {product.stock}個
-          {product.is_featured && (<span>おすすめ</span>)}
-        </div>
-      ))}
-    </main>
-  );
 }
+
+/*
+
+■ products?.map((product) => 　とは
+配列（products）の 各要素 を（product）として取り出す。
+（空になるまでループ）
+
+また、今回取り出した（product）はオブジェクトです。
+こんなん → { id:1, name:"商品A", ...}
+
+product.name　→　オブジェクト（product）のプロパティ（name）
+
+*/
