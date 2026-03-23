26342b83e8ca833e5345f7d6b383f0b732271c32
25r1116sasaki-code <25r1116.sasaki@gmail.com>
2026-03-23 16:44:58 +0900

    データベースとの接続

--- naniwa-ec/lib/supabase/client.ts
+++ naniwa-ec/lib/supabase/client.ts
@@ -2,7 +2,11 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
   return createBrowserClient(
+    // process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    // process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!=sb_publishable_IYsYXORCnCG8XYIQOw2YDQ_nWxk6n2u
   );
 }
+
+
